f94ebdd72e0f96a6d2094a7cb3e674e5f52edd46
Daniel Koh <aikhong21@gmail.com>
2023-02-22 18:19:57 +0800

    added unit tests for catalog

--- test/simpledb/CatalogTest.java
+++ test/simpledb/CatalogTest.java
@@ -2,6 +2,7 @@ package simpledb;
 
 import static org.junit.Assert.assertEquals;
 
+import java.util.Iterator;
 import java.util.NoSuchElementException;
 import java.util.Random;
 
@@ -20,15 +21,16 @@ import simpledb.systemtest.SimpleDbTestBase;
 import simpledb.systemtest.SystemTestUtil;
 
 public class CatalogTest extends SimpleDbTestBase {
-	private static final Random r = new Random();
+    private static final Random r = new Random();
     private static final String name = SystemTestUtil.getUUID();
     private static final int id1 = r.nextInt();
     private static final int id2 = r.nextInt();
-	private String nameThisTestRun;
-    
-    @Before public void addTables() {
+    private String nameThisTestRun;
+
+    @Before
+    public void addTables() {
         Database.getCatalog().clear();
-		nameThisTestRun = SystemTestUtil.getUUID();
+        nameThisTestRun = SystemTestUtil.getUUID();
         Database.getCatalog().addTable(new SkeletonFile(id1, Utility.getTupleDesc(2)), nameThisTestRun);
         Database.getCatalog().addTable(new SkeletonFile(id2, Utility.getTupleDesc(2)), name);
     }
@@ -36,7 +38,8 @@ public class CatalogTest extends SimpleDbTestBase {
     /**
      * Unit test for Catalog.getTupleDesc()
      */
-    @Test public void getTupleDesc() {
+    @Test
+    public void getTupleDesc() {
         TupleDesc expected = Utility.getTupleDesc(2);
         TupleDesc actual = Database.getCatalog().getTupleDesc(id1);
 
@@ -46,55 +49,128 @@ public class CatalogTest extends SimpleDbTestBase {
     /**
      * Unit test for Catalog.getTableId()
      */
-    @Test public void getTableId() {
+    @Test
+    public void getTableId() {
         assertEquals(id2, Database.getCatalog().getTableId(name));
         assertEquals(id1, Database.getCatalog().getTableId(nameThisTestRun));
-        
+
         try {
             Database.getCatalog().getTableId(null);
             Assert.fail("Should not find table with null name");
         } catch (NoSuchElementException e) {
             // Expected to get here
         }
-        
+
         try {
             Database.getCatalog().getTableId("foo");
             Assert.fail("Should not find table with name foo");
         } catch (NoSuchElementException e) {
             // Expected to get here
         }
+
+        // additional test
+        int tableId = Database.getCatalog().getTableId(name);
+        Assert.assertEquals(id2, tableId);
+    }
+
+    /**
+     * Unit test for Catalog.getTableName()
+     */
+    @Test
+    public void getTableName() {
+        assertEquals(name, Database.getCatalog().getTableName(id2));
+        assertEquals(nameThisTestRun, Database.getCatalog().getTableName(id1));
+
+        try {
+            Database.getCatalog().getTableName(-1);
+            Assert.fail("Should not find table with id -1");
+        } catch (NoSuchElementException e) {
+            // Expected to get here
+        }
+
+        try {
+            Database.getCatalog().getTableName(100);
+            Assert.fail("Should not find table with id 100");
+        } catch (NoSuchElementException e) {
+            // Expected to get here
+        }
+
+        // additional test
+        String tableName = Database.getCatalog().getTableName(id1);
+        Assert.assertEquals(nameThisTestRun, tableName);
     }
 
     /**
      * Unit test for Catalog.getDatabaseFile()
      */
 
-    @Test public void getDatabaseFile() {
+    @Test
+    public void getDatabaseFile() {
         DbFile f = Database.getCatalog().getDatabaseFile(id1);
 
         // NOTE(ghuo): we try not to dig too deeply into the DbFile API here; we
         // rely on HeapFileTest for that. perform some basic checks.
         assertEquals(id1, f.getId());
     }
-    
+
     /**
      * Check that duplicate names are handled correctly
      */
-    @Test public void handleDuplicateNames() {
-    	int id3 = r.nextInt();
-    	Database.getCatalog().addTable(new SkeletonFile(id3, Utility.getTupleDesc(2)), name);
-    	assertEquals(id3, Database.getCatalog().getTableId(name));
+    @Test
+    public void handleDuplicateNames() {
+        int id3 = r.nextInt();
+        Database.getCatalog().addTable(new SkeletonFile(id3, Utility.getTupleDesc(2)), name);
+        assertEquals(id3, Database.getCatalog().getTableId(name));
     }
-    
+
     /**
      * Check that duplicate file ids are handled correctly
      */
-    @Test public void handleDuplicateIds() {
-    	String newName = SystemTestUtil.getUUID();
-    	DbFile f = new SkeletonFile(id2, Utility.getTupleDesc(2));
-    	Database.getCatalog().addTable(f, newName);
-    	assertEquals(newName, Database.getCatalog().getTableName(id2));
-    	assertEquals(f, Database.getCatalog().getDatabaseFile(id2));
+    @Test
+    public void handleDuplicateIds() {
+        String newName = SystemTestUtil.getUUID();
+        DbFile f = new SkeletonFile(id2, Utility.getTupleDesc(2));
+        Database.getCatalog().addTable(f, newName);
+        assertEquals(newName, Database.getCatalog().getTableName(id2));
+        assertEquals(f, Database.getCatalog().getDatabaseFile(id2));
+    }
+
+    /**
+     * Unit test for Catalog.getPrimaryKey()
+     */
+    @Test
+    public void getPrimaryKey() {
+        assertEquals(null, Database.getCatalog().getPrimaryKey(id1));
+        assertEquals(null, Database.getCatalog().getPrimaryKey(id2));
+    }
+
+    /**
+     * Unit test for Catalog.tableIdIterator()
+     */
+    @Test
+    public void tableIdIterator() {
+        int count = 0;
+        Iterator<Integer> it = Database.getCatalog().tableIdIterator();
+        while (it.hasNext()) {
+            count++;
+            int id = it.next();
+            Assert.assertTrue(id == id1 || id == id2);
+        }
+        Assert.assertEquals(2, count);
+    }
+
+    /**
+     * Unit test for Catalog.clear()
+     */
+    @Test
+    public void clear() {
+        Database.getCatalog().clear();
+        try {
+            Database.getCatalog().getTableId(name);
+            Assert.fail("Should not find table with name " + name);
+        } catch (NoSuchElementException e) {
+            // Expected to get here
+        }
     }
 
     /**
@@ -104,4 +180,3 @@ public class CatalogTest extends SimpleDbTestBase {
         return new JUnit4TestAdapter(CatalogTest.class);
     }
 }
-
